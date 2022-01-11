4b7dd09ead46a64b8f74a225c021ce8d718c5d8e
Arun Rajora <rajora.rajora2@gmail.com>
2022-01-11 16:55:41 +0530

    Add notifications.

--- src/screens/Customer.js
+++ src/screens/Customer.js
@@ -1,4 +1,7 @@
+import { useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
 
 function Customer({ navigation }) {
   const customer = {
@@ -9,6 +12,41 @@ function Customer({ navigation }) {
     active: false,
     reminderTime: '20 January 2022, 04:44 PM',
   };
+
+  useEffect(async () => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    alert('10 seconds to go');
+    const result = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Talk to customer',
+        body: `Talk to ${customer.firstName} ${customer.lastName}`,
+        data: { data: customer },
+        sound: true,
+        vibrate: true,
+        priority: 'high',
+      },
+      trigger: new Date().getTime() + 10000,
+    });
+    alert(result);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>
