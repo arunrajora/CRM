0e4248656808f8cf34466993984caf68fe4ce511
Arun Rajora <rajora.rajora2@gmail.com>
2022-01-15 23:19:43 +0530

    Remove notifications on data storage clear.

--- src/features/Sagas.js
+++ src/features/Sagas.js
@@ -1,10 +1,19 @@
 import { call, all, takeEvery, put, takeLatest } from 'redux-saga/effects';
-import { clearDataFromStorage } from '../utilities/async_storage';
+import { clearDataFromStorage, getCustomers } from '../utilities/async_storage';
+import { removeNotification } from '../utilities/notifications';
 import { clearData as clearDataAction } from './customers/CustomerSlice';
 import { fetchRegions } from './regions/Sagas';
 import { fetchCustomers, watchSaveCustomer } from './customers/Sagas';
 
 function* clearData() {
+  const customers = yield getCustomers();
+  if (customers) {
+    for (const customer of customers) {
+      if (customer.reminderTime) {
+        yield removeNotification(customer.notificationId);
+      }
+    }
+  }
   yield clearDataFromStorage();
   yield put(clearDataAction());
 }
